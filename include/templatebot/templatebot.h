2e06056420a9ec2f39c5d579dd5c6b01e3c3efe8
Craig Edwards <craigedwards@brainbox.cc>
2021-05-09 12:21:25 +0100

    initial

---
+++ include/templatebot/templatebot.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <dpp/dpp.h>
+#include <fmt/format.h>
+#include <nlohmann/json.hpp>
+#include <dpp/discordevents.h>
+#include <dpp/message.h>
+
+// Place any forward declarations here
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <dpp/dpp.h>
+#include <fmt/format.h>
+#include <nlohmann/json.hpp>
+#include <iomanip>
+#include <sstream>
+#include <dpp/discordevents.h>
+#include <dpp/message.h>
+#include <templatebot/templatebot.h>
+
+using json = nlohmann::json;
+
+int main(int argc, char const *argv[])
+{
+    json configdocument;
+    std::ifstream configfile("../config.json");
+    configfile >> configdocument;
+
+    /* Setup the bot */
+    dpp::cluster bot(configdocument["token"]);
+
+    /* Log event */
+    bot.on_log([&bot](const dpp::log_t &event) {
+	if (event.severity >= dpp::ll_debug)
+	{
+		std::cout << dpp::utility::current_date_time() << " [" << dpp::utility::loglevel(event.severity) << "] " << event.message << "\n";
+	}
+    });
+
+    /* Use the on_message_create event to look for commands */
+    bot.on_message_create([&bot](const dpp::message_create_t &event) {
+
+	std::stringstream ss(event.msg->content);
+	std::string command;
+	ss >> command;
+
+	if (command == "!hello")
+	{
+		bot.message_create(dpp::message(event.msg->channel_id, "Bot is not playing a song"));
+	}
+
+    });
+
+    /* Start bot */
+    bot.start(false);
+    return 0;
+}
+
