728fdceece9acff7b7fca72b50fb66c7de98f2cf
es183923 <72744903+es183923@users.noreply.github.com>
2022-07-02 19:29:41 -0400

    Delete cmake directory

--- cmake/FindDPP.cmake
+++
@@ -1,7 +0,0 @@
-find_path(DPP_INCLUDE_DIR NAMES dpp/dpp.h HINTS ${DPP_ROOT_DIR})
-
-find_library(DPP_LIBRARIES NAMES dpp "libdpp.a" HINTS ${DPP_ROOT_DIR})
-
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(DPP DEFAULT_MSG DPP_LIBRARIES DPP_INCLUDE_DIR)
