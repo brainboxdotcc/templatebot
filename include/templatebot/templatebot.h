084429826602235861d29f1614602b0904852b45
Craig Edwards <braindigitalis@users.noreply.github.com>
2022-07-31 16:12:17 +0100

    fix: remove headers

--- include/templatebot/templatebot.h
+++ include/templatebot/templatebot.h
@@ -1,8 +1,6 @@
 #pragma once
+
 #include <dpp/dpp.h>
-#include <dpp/fmt/format.h>
 #include <dpp/nlohmann/json.hpp>
-#include <dpp/discordevents.h>
-#include <dpp/message.h>
 
 // Place any forward declarations here
